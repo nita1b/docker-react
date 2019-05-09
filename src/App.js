c008e759662a7646d66d4f7f77fc6fbe971cbcf6
nita1b <nabanita1b@gmail.com>
2019-05-09 13:59:14 -0400

    changed for feature

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Bye There
+          I was changed for feature branch
         </a>
       </header>
     </div>
